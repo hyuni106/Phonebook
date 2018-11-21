0dfdbc0c1cf883c90606b71eefff6bd5f2219384
suhyun <suhyuni106@hanmail.net>
2018-11-21 21:01:21 +0900

    라이브서버 주소 추가

--- app/src/main/java/kr/idealidea/phonebook/utils/ConnectServer.java
+++ app/src/main/java/kr/idealidea/phonebook/utils/ConnectServer.java
@@ -31,6 +31,7 @@ import okio.ByteString;
 
 
 public class ConnectServer {
+//    private final static String BASE_URL = "http://52.79.86.109/"; //TEST LIVE SERVER
     private final static String BASE_URL = "http://172.30.1.29:5000/";
 //    private final static String BASE_URL = "http://192.168.0.149:5000/";
 //    private final static String BASE_URL = "http://172.30.1.11:5000/";
