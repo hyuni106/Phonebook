128777d64e692993a82698f5ba62168dc52bd006
suhyun <suhyuni106@hanmail.net>
2018-11-24 16:44:46 +0900

    번호 퍼미션 처리 오레오만 따로하도록 수정

--- app/src/main/java/kr/search/phonebook/SplashActivity.java
+++ app/src/main/java/kr/search/phonebook/SplashActivity.java
@@ -3,6 +3,7 @@ package kr.search.phonebook;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
@@ -39,35 +40,42 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-        TedPermission.with(this)
-                .setPermissionListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted() {
-                        new Handler().postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                getPhoneNum();
-                                if (ContextUtils.getUserToken(mContext).equals("")) {
-                                    Intent intent = new Intent(mContext, LoginActivity.class);
-                                    intent.putExtra("phone", finalPhoneNum);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    ConnectServer.getRequestUserInfo(mContext, new ConnectServer.JsonResponseHandler() {
-                                        @Override
-                                        public void onResponse(JSONObject json) {
-                                            try {
-                                                if (json.getInt("code") == 200) {
-                                                    JSONObject user = json.getJSONObject("data").getJSONObject("user");
-                                                    GlobalData.loginUser = User.getUserFromJson(user);
-                                                    GlobalData.loginUser.setAdmin(json.getJSONObject("data").getBoolean("is_admin"));
-                                                    if (!user.isNull("period")) {
-                                                        JSONObject period = user.getJSONObject("period");
-                                                        GlobalData.loginUser.setUserPeriod(Period.getPeriodFromJson(period));
-                                                        if (GlobalData.loginUser.getUserPeriod().getEnd().getTimeInMillis() > Calendar.getInstance().getTimeInMillis()) {
-                                                            Intent intent = new Intent(mContext, MainActivity.class);
-                                                            startActivity(intent);
-                                                            finish();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            TedPermission.with(this)
+                    .setPermissionListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted() {
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    getPhoneNum();
+                                    if (ContextUtils.getUserToken(mContext).equals("")) {
+                                        Intent intent = new Intent(mContext, LoginActivity.class);
+                                        intent.putExtra("phone", finalPhoneNum);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        ConnectServer.getRequestUserInfo(mContext, new ConnectServer.JsonResponseHandler() {
+                                            @Override
+                                            public void onResponse(JSONObject json) {
+                                                try {
+                                                    if (json.getInt("code") == 200) {
+                                                        JSONObject user = json.getJSONObject("data").getJSONObject("user");
+                                                        GlobalData.loginUser = User.getUserFromJson(user);
+                                                        GlobalData.loginUser.setAdmin(json.getJSONObject("data").getBoolean("is_admin"));
+                                                        if (!user.isNull("period")) {
+                                                            JSONObject period = user.getJSONObject("period");
+                                                            GlobalData.loginUser.setUserPeriod(Period.getPeriodFromJson(period));
+                                                            if (GlobalData.loginUser.getUserPeriod().getEnd().getTimeInMillis() > Calendar.getInstance().getTimeInMillis()) {
+                                                                Intent intent = new Intent(mContext, MainActivity.class);
+                                                                startActivity(intent);
+                                                                finish();
+                                                            } else {
+                                                                Intent intent = new Intent(mContext, LoginActivity.class);
+                                                                intent.putExtra("phone", finalPhoneNum);
+                                                                startActivity(intent);
+                                                                finish();
+                                                            }
                                                         } else {
                                                             Intent intent = new Intent(mContext, LoginActivity.class);
                                                             intent.putExtra("phone", finalPhoneNum);
@@ -80,33 +88,97 @@ public class SplashActivity extends BaseActivity {
                                                         startActivity(intent);
                                                         finish();
                                                     }
-                                                } else {
-                                                    Intent intent = new Intent(mContext, LoginActivity.class);
-                                                    intent.putExtra("phone", finalPhoneNum);
-                                                    startActivity(intent);
-                                                    finish();
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
                                                 }
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
                                             }
-                                        }
-                                    });
+                                        });
+                                    }
                                 }
-                            }
-                        }, 1000);
-                    }
+                            }, 1000);
+                        }
 
-                    @Override
-                    public void onPermissionDenied(List<String> deniedPermissions) {
-                        Toast.makeText(SplashActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
-                    }
+                        @Override
+                        public void onPermissionDenied(List<String> deniedPermissions) {
+                            Toast.makeText(SplashActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                        }
 
-                })
-                .setDeniedMessage("어플을 사용하려면 권한을 허용해야 합니다.")
-                .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.READ_SMS, Manifest.permission.READ_CALL_LOG
-                        , Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_PHONE_NUMBERS, Manifest.permission.READ_SMS, Manifest.permission.SYSTEM_ALERT_WINDOW
-                        , Manifest.permission.RECEIVE_BOOT_COMPLETED, Manifest.permission.WAKE_LOCK, Manifest.permission.PROCESS_OUTGOING_CALLS)
-                .check();
+                    })
+                    .setDeniedMessage("어플을 사용하려면 권한을 허용해야 합니다.")
+                    .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.READ_SMS, Manifest.permission.READ_CALL_LOG
+                            , Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_PHONE_NUMBERS, Manifest.permission.READ_SMS, Manifest.permission.SYSTEM_ALERT_WINDOW
+                            , Manifest.permission.RECEIVE_BOOT_COMPLETED, Manifest.permission.WAKE_LOCK, Manifest.permission.PROCESS_OUTGOING_CALLS)
+                    .check();
+        } else {
+            TedPermission.with(this)
+                    .setPermissionListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted() {
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    getPhoneNum();
+                                    if (ContextUtils.getUserToken(mContext).equals("")) {
+                                        Intent intent = new Intent(mContext, LoginActivity.class);
+                                        intent.putExtra("phone", finalPhoneNum);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        ConnectServer.getRequestUserInfo(mContext, new ConnectServer.JsonResponseHandler() {
+                                            @Override
+                                            public void onResponse(JSONObject json) {
+                                                try {
+                                                    if (json.getInt("code") == 200) {
+                                                        JSONObject user = json.getJSONObject("data").getJSONObject("user");
+                                                        GlobalData.loginUser = User.getUserFromJson(user);
+                                                        GlobalData.loginUser.setAdmin(json.getJSONObject("data").getBoolean("is_admin"));
+                                                        if (!user.isNull("period")) {
+                                                            JSONObject period = user.getJSONObject("period");
+                                                            GlobalData.loginUser.setUserPeriod(Period.getPeriodFromJson(period));
+                                                            if (GlobalData.loginUser.getUserPeriod().getEnd().getTimeInMillis() > Calendar.getInstance().getTimeInMillis()) {
+                                                                Intent intent = new Intent(mContext, MainActivity.class);
+                                                                startActivity(intent);
+                                                                finish();
+                                                            } else {
+                                                                Intent intent = new Intent(mContext, LoginActivity.class);
+                                                                intent.putExtra("phone", finalPhoneNum);
+                                                                startActivity(intent);
+                                                                finish();
+                                                            }
+                                                        } else {
+                                                            Intent intent = new Intent(mContext, LoginActivity.class);
+                                                            intent.putExtra("phone", finalPhoneNum);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        }
+                                                    } else {
+                                                        Intent intent = new Intent(mContext, LoginActivity.class);
+                                                        intent.putExtra("phone", finalPhoneNum);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+                            }, 1000);
+                        }
+
+                        @Override
+                        public void onPermissionDenied(List<String> deniedPermissions) {
+                            Toast.makeText(SplashActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                        }
+
+                    })
+                    .setDeniedMessage("어플을 사용하려면 권한을 허용해야 합니다.")
+                    .setPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.READ_SMS, Manifest.permission.READ_CALL_LOG
+                            , Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_SMS, Manifest.permission.SYSTEM_ALERT_WINDOW
+                            , Manifest.permission.RECEIVE_BOOT_COMPLETED, Manifest.permission.WAKE_LOCK, Manifest.permission.PROCESS_OUTGOING_CALLS)
+                    .check();
+        }
 
     }
 
